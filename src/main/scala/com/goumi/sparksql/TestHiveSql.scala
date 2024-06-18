6af9bfa5ac9815d7820efeefcb2b42dc56e73abb
Gougogo <1572158030@qq.com>
2024-06-18 19:11:18 +0800

    add

--- src/main/scala/com/goumi/sparksql/TestHiveSql.scala
+++ src/main/scala/com/goumi/sparksql/TestHiveSql.scala
@@ -8,6 +8,7 @@ import org.apache.spark.sql.SparkSession
  */
 object TestHiveSql {
   def main(args: Array[String]): Unit = {
+
     val spark: SparkSession = SparkSession.builder()
       .appName(this.getClass.getSimpleName)
       .enableHiveSupport()
